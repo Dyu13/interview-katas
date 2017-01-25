d2d9b48fbca598fb563d31a19dff2e3c5e625072
Ovidiu Giurgiu <ovidiu.giurgiu_93@outlook.com>
2017-01-25 11:29:08 +0200

    project structure

---
+++ CheckoutKata/CheckoutKata.ClientAdminUWP/Setup.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Controls;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.Platform;
+using MvvmCross.WindowsUWP.Platform;
+
+namespace CheckoutKata.ClientAdminUWP
+{
+    public class Setup : MvxWindowsSetup
+    {
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+    }
+}
